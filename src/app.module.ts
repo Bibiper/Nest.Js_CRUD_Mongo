d443c2d4ff3d67d46ea5f8a3cececaa935659fff
Bianca Moraes <67522146+Bibiper@users.noreply.github.com>
2021-12-07 11:04:23 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://biancasm:iKkX2WKtgjCu4u7@cluster-nestjs-crud.03vp6.mongodb.net/test'),UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://<username>:<password>@cluster-nestjs-crud.03vp6.mongodb.net/test'),UsersModule],
   controllers: [],
   providers: [],
 })
